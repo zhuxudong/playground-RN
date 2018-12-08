c972fbe175f5a25f4d328d46d3a98b2569b7b839
zhuxudong <callzhuxudong@163.com>
2018-12-08 15:32:16 +0800

    部分页面取消底部导航栏

--- src/Component/NavBottom/index.js
+++ src/Component/NavBottom/index.js
@@ -7,120 +7,131 @@ import I18n from "../../common/i18n"
  * @param {object} containerStyle - 容器样式
  * */
 export default class NavBottom extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            activeIndex: 0
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeIndex: 0,
+      navShow: true
     }
+  }
 
-    pick(index) {
-        this.setState({
-            activeIndex: index
-        }, () => {
-            let componentName = "";
-            switch (index) {
-                case 0:
-                    componentName = "Home";
-                    break;
-                case 1:
-                    componentName = "Classify";
-                    break;
-                case 2:
-                    componentName = "Favorite";
-                    break;
-                case 3:
-                    componentName = "My";
-                    break;
+  pick(index) {
+    this.setState({
+      activeIndex: index
+    }, () => {
+      let componentName = "";
+      switch (index) {
+        case 0:
+          componentName = "Home";
+          break;
+        case 1:
+          componentName = "Classify";
+          break;
+        case 2:
+          componentName = "Favorite";
+          break;
+        case 3:
+          componentName = "My";
+          break;
+      }
+      navigation.navigate(componentName)
+    })
+  }
+
+  highLight(index) {
+    this.setState({
+      activeIndex: index
+    })
+  }
+
+  showNavBottom(show) {
+    this.setState({
+      navShow: show
+    })
+  }
+
+  render() {
+    return (
+      <View style={[{...style.container, ...this.props.containerStyle}, this.state.navShow && style.show]}>
+        <TouchableWithoutFeedback onPress={this.pick.bind(this, 0)}>
+          <View style={style.item}>
+            {
+              this.state.activeIndex === 0 ?
+                <Image style={style.navIcon} source={require("./img/home_active.png")}/> :
+                <Image style={style.navIcon} source={require("./img/home.png")}/>
             }
-            navigation.navigate(componentName)
-        })
-    }
-
-    highLight(index) {
-        this.setState({
-            activeIndex: index
-        })
-    }
-
-    render() {
-        return (
-            <View style={{...style.container, ...this.props.containerStyle}}>
-                <TouchableWithoutFeedback onPress={this.pick.bind(this, 0)}>
-                    <View style={style.item}>
-                        {
-                            this.state.activeIndex === 0 ?
-                                <Image style={style.navIcon} source={require("./img/home_active.png")}/> :
-                                <Image style={style.navIcon} source={require("./img/home.png")}/>
-                        }
-                        <Text
-                            style={[style.text, this.state.activeIndex === 0 && style.activeText]}>{I18n.t("navBottom.homeIconName")}</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={this.pick.bind(this, 1)}>
-                    <View style={style.item}>
-                        {
-                            this.state.activeIndex === 1 ?
-                                <Image style={style.navIcon} source={require("./img/classify_active.png")}/> :
-                                <Image style={style.navIcon} source={require("./img/classify.png")}/>
-                        }
-                        <Text
-                            style={[style.text, this.state.activeIndex === 1 && style.activeText]}>{I18n.t("navBottom.classifyIconName")}</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={this.pick.bind(this, 2)}>
-                    <View style={style.item}>
-                        {
-                            this.state.activeIndex === 2 ?
-                                <Image style={style.navIcon} source={require("./img/favorite_active.png")}/> :
-                                <Image style={style.navIcon} source={require("./img/favorite.png")}/>
-                        }
-                        <Text
-                            style={[style.text, this.state.activeIndex === 2 && style.activeText]}>{I18n.t("navBottom.favoriteIconName")}</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={this.pick.bind(this, 3)}>
-                    <View style={style.item}>
-                        {
-                            this.state.activeIndex === 3 ?
-                                <Image style={style.navIcon} source={require("./img/my_active.png")}/> :
-                                <Image style={style.navIcon} source={require("./img/my.png")}/>
-                        }
-                        <Text
-                            style={[style.text, this.state.activeIndex === 3 && style.activeText]}>{I18n.t("navBottom.myIconName")}</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-            </View>
-        )
-    }
+            <Text
+              style={[style.text, this.state.activeIndex === 0 && style.activeText]}>{I18n.t("navBottom.homeIconName")}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={this.pick.bind(this, 1)}>
+          <View style={style.item}>
+            {
+              this.state.activeIndex === 1 ?
+                <Image style={style.navIcon} source={require("./img/classify_active.png")}/> :
+                <Image style={style.navIcon} source={require("./img/classify.png")}/>
+            }
+            <Text
+              style={[style.text, this.state.activeIndex === 1 && style.activeText]}>{I18n.t("navBottom.classifyIconName")}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={this.pick.bind(this, 2)}>
+          <View style={style.item}>
+            {
+              this.state.activeIndex === 2 ?
+                <Image style={style.navIcon} source={require("./img/favorite_active.png")}/> :
+                <Image style={style.navIcon} source={require("./img/favorite.png")}/>
+            }
+            <Text
+              style={[style.text, this.state.activeIndex === 2 && style.activeText]}>{I18n.t("navBottom.favoriteIconName")}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={this.pick.bind(this, 3)}>
+          <View style={style.item}>
+            {
+              this.state.activeIndex === 3 ?
+                <Image style={style.navIcon} source={require("./img/my_active.png")}/> :
+                <Image style={style.navIcon} source={require("./img/my.png")}/>
+            }
+            <Text
+              style={[style.text, this.state.activeIndex === 3 && style.activeText]}>{I18n.t("navBottom.myIconName")}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
+  }
 }
 
 const style = StyleSheet.create({
-    container: {
-        height: 50,
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        borderTopColor: "#F1F1F1",
-        borderTopWidth: 1
-    },
-    item: {
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    navIcon: {
-        width: 20,
-        height: 20,
-        resizeMode: "contain"
-    },
-    activeNavIcon: {},
-    text: {
-        marginTop: 5,
-        fontSize: 10,
-        color: "#4A4A4A"
-    },
-    activeText: {
-        color: "#F15947"
-    }
+  container: {
+    height: 50,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderTopColor: "#F1F1F1",
+    borderTopWidth: 1,
+    display: "none"
+  },
+  show: {
+    display: "flex"
+  },
+  item: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  navIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: "contain"
+  },
+  activeNavIcon: {},
+  text: {
+    marginTop: 5,
+    fontSize: 10,
+    color: "#4A4A4A"
+  },
+  activeText: {
+    color: "#F15947"
+  }
 });
--- src/router/navigationService.js
+++ src/router/navigationService.js
@@ -4,38 +4,48 @@ let _navigator;
 let _navBottom;
 
 function setTopLevelNavigator(navigatorRef) {
-    _navigator = navigatorRef;
+  _navigator = navigatorRef;
 }
 
 function setNavBottom(navBottom) {
-    _navBottom = navBottom;
+  _navBottom = navBottom;
 }
 
 function navigate(routeName, params) {
-    _navigator.dispatch(
-        NavigationActions.navigate({
-            routeName,
-            params,
-        })
-    );
-    switch (routeName) {
-        case "Home":
-            _navBottom.highLight(0);
-            break;
-        case "Classify":
-            _navBottom.highLight(1);
-            break;
-        case "Favorite":
-            _navBottom.highLight(2);
-            break;
-        case "My":
-            _navBottom.highLight(3);
-            break;
-    }
+  _navigator.dispatch(
+    NavigationActions.navigate({
+      routeName,
+      params,
+    })
+  );
+  switch (routeName) {
+    case "Home":
+      _navBottom.highLight(0);
+      break;
+    case "Classify":
+      _navBottom.highLight(1);
+      break;
+    case "Favorite":
+      _navBottom.highLight(2);
+      break;
+    case "My":
+      _navBottom.highLight(3);
+      break;
+  }
+  switch (routeName) {
+    case "Home":
+    case "Classify":
+    case "Favorite":
+    case "My":
+      _navBottom.showNavBottom(true)
+      break;
+    default:
+      _navBottom.showNavBottom(false)
+  }
 }
 
 export default {
-    setTopLevelNavigator,
-    setNavBottom,
-    navigate,
+  setTopLevelNavigator,
+  setNavBottom,
+  navigate,
 };
